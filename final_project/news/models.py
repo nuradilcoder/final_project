6c8d343ffba8e3fa7ac7d14c62837ebbee0e1244
nuradilcoder <kulubaevnuradil2@gmail.com>
2025-07-28 09:40:27 +0600

    Add files via upload

--- final_project/news/models.py
+++ final_project/news/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
-
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    views_count = models.IntegerField(default=0 ,verbose_name='Просмотры')
+    news_image = models.ImageField(upload_to='news_image/',blank=True,null=True,verbose_name='Новостное изображение')
+    
     def __str__(self):
         return self.title
+    
+    
+
+    
--- final_project/news/views.py
+++ final_project/news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News
 
 # Create your views here.
+def news_list(request):
+    news = News.objects.all()
+    context = {'news': news}
+    return render(request, 'news_list.html', context) 
+
+def news_detail(request, id):
+    news = get_object_or_404(News, id=id)
+    context = {'news' : news}
+    return render(request, 'news_detail.html', context )
